941ce9ffec08674bdc0494e37b91f77000cb453e
ds18b20 <858362490@qq.com>
2017-10-29 19:17:54 +0900

    20171029 add cnn

--- activators.py
+++ activators.py
@@ -14,3 +14,20 @@ class SigmoidActivator(object):
 
     def backward(self, output):
         return output * (1 - output)
+
+
+class ReluActivator(object):
+    def forward(self, weighted_input):
+        # return weighted_input
+        return max(0, weighted_input)
+
+    def backward(self, output):
+        return 1 if output > 0 else 0
+
+
+class IdentityActivator(object):
+    def forward(self, weighted_input):
+        return weighted_input
+
+    def backward(self, output):
+        return 1
