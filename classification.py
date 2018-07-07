63ea6e1bc5e7221be7c9c6600d487dfb39873b9b
ds18b20 <858362490@qq.com>
2018-07-07 15:55:28 +0900

    20180707 classification update

--- classification.py
+++ classification.py
@@ -4,58 +4,101 @@ import datasets
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class Classification(object):
-    def __init__(self):
-        pass
+    def __init__(self, weights, bias, learning_rate):
+        self.W = weights  # (28*28, 10)
+        self.b = bias  # (10,)
+        self.lr = learning_rate
+    
+        self.fc_out = None
+        self.sm_out = None
+        self.ce_out = None
+    
+        self.d_W = None
+        self.d_b = None
+    
+        self.d_fc_out = None
+    
+    def net(self, input_batch):
+        """
+        calculate fully-connected layer
+        :param input_batch: (5, 28*28)
+                    self.W: (28*28, 10)
+        :return: X dot W + b = (5, 10)
+        """
+        return np.dot(input_batch, self.W) + self.b  # (5, 10)
+
+    def forward(self, input_batch, label_batch):
+        """
+        forward calculate & store mediate nodes value
+        :param input_batch: (5, 28*28)
+        :param label_batch: (5,)
+        :return: None
+        """
+        self.fc_out = self.net(input_batch)  # (5, 10)
+        self.sm_out = softmax(self.fc_out)  # (5, 10)
+        self.ce_out = cross_entropy(self.sm_out, label_batch)  # (1,)
+    
+    def bp_softmax_cross_entropy(self, labels):
+        """
+        softmax_cross_entropy back propagation
+        :param labels: (5,)
+        :return: None
+        """
+        if labels.ndim == 1:
+            self.d_fc_out = self.sm_out - datasets.one_hot(labels)  # (5, 10)
+        elif labels.ndim == 2:
+            self.d_fc_out = self.sm_out - labels  # (5, 10)
+
+    def bp_fc(self, input_batch):
+        """
+        fully-connected back propagation
+        :param input_batch: (5, 28*28)
+        :return: None
+        """
+        self.d_W = np.dot(input_batch.T, self.d_fc_out)  # (28*28, 10)
+        self.d_b = np.sum(self.d_fc_out, axis=0, keepdims=False)  # (10,)
+    
+    def backward(self, input_batch, labels):
+        """
+        backward calculate & update weights and bias
+        :param input_batch: (5, 28*28)
+        :param labels: (5,)
+        :return: None
+        """
+        self.bp_softmax_cross_entropy(labels)  # (5， 10)
+        self.bp_fc(input_batch)
+
+    def update_para(self):
+        self.W -= self.lr * self.d_W
+        self.b -= self.lr * self.d_b
 
 
-def net(X, W, b):
-    return np.dot(X, W) + b
-
-
-def softmax(X):
-    X = X - X.max(axis=1, keepdims=True)
-    exp = np.exp(X)
+def softmax(array):
+    array -= array.max(axis=1, keepdims=True)  # max of array in axis 1
+    exp = np.exp(array)
     return exp / np.sum(exp, axis=1, keepdims=True)
 
 
-def epoch_accuracy(data_iter, net):
-    acc = 0
-    for X, y in data_iter:
-        acc += accuracy(net(X), y)
-    return acc / len(data_iter)
-
-
-def forward(X, W, b, labels):
-    fc = net(X, W, b)
-    sm = softmax(fc)
-    ce = cross_entropy(sm, labels)
-    return ce
-
-
-def backward(X, W, b):
-    """
-    backward function for model
-    :param X:
-    :param W:
-    :param b:
-    :return:
-    """
-    pass
-
-
 def cross_entropy(y_hat, y):
-    index_row = range(y_hat.shape[0])
+    delta = 1e-6  # not log(0)
+    row_count = y_hat.shape[0]
+    index_row = range(row_count)
     index_column = y
-    picked = y_hat[index_row, index_column]
-    return np.sum(-np.log(picked))
-    
-    
+    picked = y_hat[index_row, index_column] + delta
+    return np.sum(-np.log(picked)) / row_count
+
+
 def accuracy(y_hat: np.array, y: np.array):
     return (y_hat.argmax(axis=1) == y).mean()
 
 
-
+# def epoch_accuracy(self, data_iter, net):
+#     acc = 0
+#     for X, y in data_iter:
+#     acc += accuracy(net(X), y)
+#     return acc / len(data_iter)
 
 def show_fashion_imgs(images, titles):
     n = images.shape[0]
@@ -72,17 +115,24 @@ def show_fashion_imgs(images, titles):
 if __name__ == '__main__':
     mnist = datasets.MNIST()
     train_x, train_y, test_x, test_y = mnist.load(image_flat=True, label_one_hot=False)
-    sample_x, sample_y = datasets.get_one_batch(train_x, train_y, batch_size=5)
-    # show_fashion_imgs(sample_x, sample_y)
-    # print(softmax(np.arange(12).reshape((3,4))))
-    P = np.array([[0.1, 0.2, 0.7], [0.2, 0.3, 0.5]])
-    L = np.array([2, 1])
-    print(cross_entropy(P, L))
-    print(np.sum(-np.log([0.7, 0.3])))
-    print(accuracy(P, L))
-    
-    num_inputs, num_outputs = 28*28, 10
+
+    # show_fashion_imgs(sample_train_x, sample_train_y)
+
+    num_inputs, num_outputs = 28 * 28, 10
     W = np.random.normal(loc=0.0, scale=0.1, size=(num_inputs, num_outputs))
     b = np.zeros(num_outputs)
-    
-    print(net(sample_x, W, b).shape)
+    op = Classification(W, b, learning_rate=0.01)
+    for _ in range(1000):
+        sample_train_x, sample_train_y = datasets.get_one_batch(train_x, train_y, batch_size=5)
+        sample_test_x, sample_test_y = datasets.get_one_batch(test_x, test_y, batch_size=5)
+        op.forward(sample_train_x, sample_train_y)
+        # print(op.fc_out.shape)
+        # print(op.sm_out.shape)
+        # print(op.ce_out.shape)
+        # print(op.ce_out)
+        op.backward(sample_train_x, sample_train_y)
+        op.update_para()
+        if _ % 50 == 0:
+            acc = accuracy(op.net(test_x), test_y)
+            print("accuracy: {}".format(acc))
+
